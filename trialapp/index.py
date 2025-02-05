3b97471b8395a911a134539987876ddb28cfe15c
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-02-05 15:49:17 +0530

    xyz

--- trialapp/index.py
+++ trialapp/index.py
@@ -1,19 +1,16 @@
 import random
 
-def generate_number():
-    return random.randint(1, 100)
 
-def guess(num, number):
+def guess(num):
+    number = random.randint(1, 100)
     if num == number:
         return "You have guessed it right!"
     else:
         return f"You have guessed it wrong! The number was {number}"
 
-# Generate the number once
-number = generate_number()
 
 # Get user input
 num = int(input("Enter a number between 1 to 100: "))
 
 # Call the function with both arguments
-print(guess(num, number))
+print(guess(num))
