984f0d6f715414689d737c8f33df2cb93a7d69b0
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-02-14 16:15:02 +0530

    maded some changes

--- beautifulsoup/object1.py
+++ beautifulsoup/object1.py
@@ -1,11 +1,17 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
-url = requests.get("https://www.bbc.com/news")
-page = urlopen('https://www.bbc.com/news')
+url = requests.get("https://thehackernews.com/")
+page = urlopen('https://thehackernews.com/')
 soup = BeautifulSoup(page, "html.parser")
+# to extact the top 10 headlines from hacker news
+headdlines = soup.select('.left-box h2')
+for i in headdlines [:10]:
+    print(i.get_text())
 #print(soup.prettify()) # this pretiffy function helps in formating the document
-print(soup.title)
-print(soup.title.string)
-print(soup.a)
-print(soup.find_all('a')) # this will print all the anchor tags
+#print(soup.title)
+#print(soup.title.string)
+#print(soup.a)
+#print(soup.find_all('a')) # this will print all the anchor tags
+
+        
