a596a05a6dd5ede1ef138853c6beabc3b00bfaa9
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-02-15 10:39:26 +0530

    added disk related info

---
+++ psutil/disk_info.py
@@ -0,0 +1,15 @@
+import psutil
+
+#finding out disk partition
+print(psutil.disk_partitions())
+
+# finding out disk usage
+print(psutil.disk_usage('/'))
+
+#getting disk io statistics
+print(psutil.disk_io_counters(perdisk=True))
+print(psutil.disk_io_counters(perdisk=False))
+
+# getting disk read write statistics
+print(psutil.disk_io_counters())
+
